1debf562d726b6fb0a56c816832dd4e6b7d2743c
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-09-03 13:39:09 -0500

    Menu-sideBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,95 @@
 import React from 'react';
-import {ListRecommendationService} from './services/ListRecomendationService';
-import FilterSection from './components/FilterSection';
+import logo from './logo.svg';
+import './App.css';
+import {Panel} from './components/Panel';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import PruebaReactIndex from './components/PruebaReactIndex';
+import PruebaReactIndex2 from './components/PruebaReactIndex2'
 
-function App() {
-  return (
-    <div className="App" style={{backgroundColor:"#B3B8E0"}}>
-      <FilterSection />
-      <ListRecommendationService />
-    </div>
-  );
+
+const routes = [
+  {
+    path: "/",
+    name: "pruebaReactIndex",
+  },
+  {
+    path: "/pruebaReactIndex2",
+    name: "pruebaReactIndex2",
+  }
+];
+
+export default class  App extends React.Component {
+
+  constructor(props){
+
+      super(props);
+
+      const pruebaReactIndex = () => (
+          <PruebaReactIndex />
+      );
+
+      const pruebaReactIndex2 = () => (
+          <PruebaReactIndex2 />
+      );
+
+
+      this.state={currentPageComponent:pruebaReactIndex}
+      this.pages={
+        "pruebaReactIndex":pruebaReactIndex,
+        "pruebaReactIndex2":pruebaReactIndex2
+      };
+      this.handleSelectedItem=this.handleSelectedItem.bind(this);
+
+  }
+
+  handleSelectedItem(itemComponent){
+    this.setState({currentPageComponent: this.pages[itemComponent]});
+  }
+
+
+  render(){
+    const isLoggedIn = true;
+    var page;
+
+
+    if(!isLoggedIn){
+      page = (
+        <div>
+
+          <div>
+            Not logged
+            {//<Route exact path="/" component={actualPage.componentValue}/>
+        }
+          </div>
+        </div>
+      );
+    }
+    else{
+      page = (
+        <div>
+
+          <div>
+            <Route  component={this.state.currentPageComponent}/>
+          </div>
+        </div>
+      );
+
+    }
+
+    return (
+
+      <div>
+        <Panel menuList={routes} handleMenuSelection={this.handleSelectedItem} />
+        <Router>
+            <div className="generalContainer">
+              {page}
+
+            </div>
+        </Router>
+      </div>
+
+    );
+  }
 }
-
-export default App;
