1c60cc16ad889456e6c0a12adf8089aea2d9e0d8
urobs17 <michael.preciado@mail.escuelaing.edu.co>
2020-09-07 16:03:43 -0500

    filter section almost finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRecommendation from "./components/createRecomendation/CreateRecommendation";
 import AddCategory from "./components/createRecomendation/AddCategory";
-
-
-const testInfo= {
+import { ListRecommendationService } from "./services/ListRecomendationService"
+import FilterSection from "./components/FilterSection"
+const testInfo = {
   title: "Las crónicas de Narnia: El león, la bruja y el ropero",
-  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum velit dui, in vehicula mi accumsan id. Suspendisse potenti. Duis id cursus velit. Praesent dapibus arcu eu dolor aliquet euismod. Sed congue nunc vehicula est molestie, a egestas lacus congue. Quisque finibus cursus justo, et scelerisque nibh. Suspendisse eget orci sit amet massa vehicula dictum. Curabitur et erat nunc. Vestibulum leo orci, dapibus eu finibus eget, ultrices non lectus. Etiam eu congue mi. Phasellus accumsan nisl vel vehicula tincidunt. Curabitur rhoncus arcu sed facilisis porttitor. Etiam tortor nisi, dictum ornare lorem id, ornare vulputate augue. Fusce tincidunt nibh ut mollis tempus. Quisque euismod, turpis ut mattis mollis, est nisi accumsan ex, ac elementum est erat id velit. Integer non nunc sed felis volutpat hendrerit id ac nisi.",
-  sourceImage:"/img/test.png",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum velit dui, in vehicula mi accumsan id. Suspendisse potenti. Duis id cursus velit. Praesent dapibus arcu eu dolor aliquet euismod. Sed congue nunc vehicula est molestie, a egestas lacus congue. Quisque finibus cursus justo, et scelerisque nibh. Suspendisse eget orci sit amet massa vehicula dictum. Curabitur et erat nunc. Vestibulum leo orci, dapibus eu finibus eget, ultrices non lectus. Etiam eu congue mi. Phasellus accumsan nisl vel vehicula tincidunt. Curabitur rhoncus arcu sed facilisis porttitor. Etiam tortor nisi, dictum ornare lorem id, ornare vulputate augue. Fusce tincidunt nibh ut mollis tempus. Quisque euismod, turpis ut mattis mollis, est nisi accumsan ex, ac elementum est erat id velit. Integer non nunc sed felis volutpat hendrerit id ac nisi.",
+  sourceImage: "/img/test.png",
   score: 4.7,
   link: "https://youtu.be/dQw4w9WgXcQ",
   author: {
@@ -16,15 +16,18 @@ const testInfo= {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     profileImg: "/img/profile_image.jpg"
   },
-  categories: ["gameplay","gameplay", "videogame", "FFVII", "GPB", "gameplay", "videogame", "FFVII", "GPB"],
+  categories: ["gameplay", "gameplay", "videogame", "FFVII", "GPB", "gameplay", "videogame", "FFVII", "GPB"],
 }
 function App() {
+  document.body.style = 'background: #B3B8E0;';
   return (
       <BrowserRouter>
-          <Switch>
-              <Route path="/" exact component={CreateRecommendation}/>
-              <Route path="/cat" exact component={AddCategory}/>
-          </Switch>
+        <FilterSection />
+        <Switch>
+          <Route path="/" exact component={ListRecommendationService} />
+          <Route path="/create-recommendation" exact component={CreateRecommendation} />
+          <Route path="/add-category" exact component={AddCategory} />
+        </Switch>
       </BrowserRouter>
   )
 }
--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,14 +1,82 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Grid, Box, TextField, InputAdornment, Chip } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const useStyles = makeStyles({
-
+    filterStyle: {
+        marginTop: "1rem",
+        padding: "0.7rem",
+    },
+    TextFieldStyle: {
+        borderWidth: '10px',
+        borderColor: 'green !important'
+    },
 });
+const CssTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: '#242847',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#242847',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#242847',
+            },
+            '&:hover fieldset': {
+                borderColor: '#242847',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#242847',
+            },
+        },
+    },
+})(TextField);
 function FilterSection(props) {
     const classes = useStyles();
+    let listCategories = ["videos", "gameplay", "videogames", "reddit"]
     return (
         <Grid container spacing={1}>
+            <Grid item xs={1} ></Grid>
+            <Grid item xs={8} >
+                <Box border={2} borderColor="primary.main" className={classes.filterStyle} borderRadius="borderRadius" >
+
+                    <CssTextField id="outlined-basic" label="Search" variant="outlined"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Autocomplete
+                        multiple
+                        limitTags={2}
+                        id="categories"
+                        size="small"
+                        options={listCategories}
+                        renderTags={(value, getTagProps) =>
+                            value.map((option, index) => (
+                                <Chip
+                                    variant="outlined"
+                                    label={option}
+                                    size="small"
+                                    clickable 
+                                    color="primary" 
+                                    {...getTagProps({ index })}
+                                />
+                            ))
+                        }
+                        renderInput={(params) => (
+                            <TextField {...params} variant="outlined" label="Filters" placeholder="Category" />
+                        )}
+                    />
+                </Box>
+            </Grid>
         </Grid>
     );
 }
