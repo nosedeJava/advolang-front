46ff7dfc63636abce3fc9b2b8c0383e3f357884a
migue1994 <migue24co@gmail.com>
2020-09-06 23:15:27 -0500

    remove menu

--- src/Menu/src/components/ListItemLink.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-
-
-export default function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
